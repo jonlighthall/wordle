e20becfa8113e60bfd4c73fd5374fb3a281b6116
Jon Lighthall <jon.lighthall@gmail.com>
2023-01-18 11:58:49 -0600

    add first try to calculate most likely values

--- src/fortran/wordle.f90
+++ src/fortran/wordle.f90
@@ -1,10 +1,11 @@
 ! beat wordle
 program wordle
   implicit none
-  integer(kind=4) :: read_unit,ierr,i,nlines = 0
-  integer(kind=4),dimension(5) :: write_unit
-  integer(kind=8),dimension(5) :: prob=0
-  character(len=5) :: line
+  integer(kind=4) :: read_unit,ierr,i,nlines = 0,ilet
+  integer(kind=4),dimension(5) :: write_unit,iprob = 0
+  integer(kind=8),dimension(5) :: isum = 0
+  real,dimension(5) :: rprob = 0.
+  character(len=5) :: line,word
 
   ! read dictionary file of 5-letter words
   open(newunit=read_unit,file='words_alpha5.txt',iostat=ierr,status='old',action='read')
@@ -21,12 +22,14 @@ program wordle
            print*,line
         endif
         do i=1,5
-           prob(i) = ichar(line(i:i),kind(prob(i)))
-           if (mod(nlines,1000).eq.0) then
-              print*,i,' = ',prob(i)
-           endif
-           if ((prob(i).lt.97).or.(prob(i).gt.122)) then
+           ilet=ichar(line(i:i),kind(ilet))
+           if ((ilet.lt.97).or.(ilet.gt.122)) then
               print*,line
+           endif
+           isum(i) = isum(i) + int(ilet,kind(isum(i)))
+           ! no, you need 26x5 array and take max value
+           if (mod(nlines,1000).eq.0) then
+              print*,i,' = ',ilet
            endif
         enddo
         nlines = nlines + 1
@@ -34,6 +37,12 @@ program wordle
         print'(/a)','EOF'
         print 1,'      number of lines = ',nlines
 1       format(a,i7)
+        do i=1,5
+           rprob(i) = real(isum(i))/real(nlines)
+           iprob(i) = nint(rprob(i))
+           word(i:i) = char(iprob(i))
+        enddo
+        print*,word
         exit
      endif
   enddo
