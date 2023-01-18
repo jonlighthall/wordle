9869ec898cf17b034e8d002c52d462bccf8019db
Jon Lighthall <jon.lighthall@gmail.com>
2023-01-18 11:12:03 -0600

    add write to filter

--- src/fortran/filter-list.f90
+++ src/fortran/filter-list.f90
@@ -1,17 +1,20 @@
+! read a dictionary file and filter for length 5
+
 program filter
   implicit none
-  integer(kind=4) :: read_unit,ierr
+  integer(kind=4) :: read_unit,ierr,write_unit
   character(len=64) :: line
-  integer :: nlines = 0
+  integer :: nlines = 0, n5 = 0
   integer :: length,mxlen = 0
 
   open(newunit=read_unit,file='words_alpha.txt',iostat=ierr,status='old',action='read')
-  print*,'iostat = ',ierr
+  print *,'iostat = ',ierr
   if (ierr==0) then
      print*,'OK'
   else
      print*,'ERROR'
   endif
+  open(newunit=write_unit,file='words_alpha5.txt')
   do
      read(read_unit,'(a)',iostat=ierr) line
      length = len(trim(line))
@@ -21,13 +24,19 @@ program filter
      nlines = nlines + 1
      if (mod(nlines,1000)==0) then
         print*,line
-     endif     
+     endif
+     if (length.eq.5) then
+        n5 = n5 +1
+        write(write_unit,'(a)') line
+     endif
      if (ierr<0) then
-        print*,'EOF'
-        print*,'number of lines = ',nlines
-        print*,'longest word length = ',mxlen
+        print'(/a)','EOF'
+        print 1,'    number of lines = ',nlines
+        print 1,'longest word length = ',mxlen
+1       format(a,i7)
         exit
      endif
   enddo
   close(read_unit)
+  close(write_unit)
 end program filter
